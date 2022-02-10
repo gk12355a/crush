417d6b0741c41aeb5349ad8663e0ce8b17e5651d
gk12355a <99313253+gk12355a@users.noreply.github.com>
2022-02-10 15:40:58 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Hi Linh </br>  Llinh ăn cứt :3",
+  titleHeader: "Hi Linh </br> Làm ny tớ nhé :3",
   descriptionHeader:
     "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "hí hí ",
   descriptionModar:
-    ' "Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> hê hê trêu tí thôi chứ nói thật đấy :) </br> Mặc dù biết thích ai đó hoặc yêu đương gì ở tuổi này nó ko hợp lí cho lắm nhưng mà t vẫn tỏ tình </br>  Ai mà biết được chắc tại thích quá :v </br> Dù cậu có từ chối thì hi vọng chúng ta vẫn làm bạn </br> lâu lâu  mới có dịp nên là xin phép sến tí gì căng :3 "',
+    ' "Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> hê hê trêu tí thôi chứ nói thật đấy :) </br> Mặc dù biết thích ai đó hoặc yêu đương gì ở tuổi này nó ko hợp lí cho lắm nhưng mà t vẫn tỏ tình </br>  Ai mà biết được chắc tại thích Linh quá :v </br> Nhiều khi cũng muốn nhắn tin mà lại ko biết nhắn gì </br> Dù cậu có từ chối thì hi vọng chúng ta vẫn làm bạn </br> lâu lâu  mới có dịp nên là xin phép sến tí gì căng :3 "',
 };
