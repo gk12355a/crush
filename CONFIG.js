d73df77920b6a101f78455946c873580bbd75f9d
gk12355a <99313253+gk12355a@users.noreply.github.com>
2023-03-08 00:01:06 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Hi Linh </br> Làm ny tớ nhé :3",
+  titleHeader: "Hi HUYỀN </br> Chúc quỳnh 8/3 vui vỏe  :3",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
+    "Đọc chút tâm tư của tôi đi ^^",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "hí hí ",
   descriptionModar:
-    ' "Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> hê hê trêu tí thôi chứ nói thật đấy :) </br> Mặc dù biết thích,yêu đương gì ở tuổi này nó ko hợp lí cho lắm nhưng mà t vẫn tỏ tình </br>  Ai mà biết được </br>  Chắc tại thích Linh nhiều quá </br> Lắm lúc cũng muốn nhắn tin mà lại ko biết nhắn gì :(  </br> " Mưa ngấm vào đất  </br> Đất ngấm ngầm cho cây </br> Mình cũng ngất ngây  </br> Ngấm vào Linh đấy " </br> Dù cậu có từ chối thì hi vọng chúng ta vẫn làm bạn </br> Lâu lâu  mới có dịp nên là xin phép sến tí gì căng :)))) </br> No matter where you are in an no matter who you are with.I will always stay here honestly truly love you "',
+    ' "Ngày còn lowkey, ít quan hệ, không chơi bời, dù có thích ai cũng khó thành chuyện yêu đương, nhát, nghĩ rằng nếu mà highkey hơn chắc sẽ tốt<\br>Ngày chọn highkey, xã giao nhiều, thích là có quan hệ, không lêu lổng nhưng cũng chẳng chịu ở nhà, thế mà vẫn cứ thích người xưa<\br>Không nổi bật nhưng cũng chẳng vô danh, vậy mà trái tim lại không đổi, hoá ra là nhận định sai, vốn dĩ ở bản thân mình, người ta đã không thích, lên voi xuống chó ba chìm bảy nổi vẫn là không thích. Chỉ là, tưởng được nhiều người biết đến thì sẽ xuất hiện nhiều người như em thôi, nhưng em quá đỗi đặc biệt, nhỉ? "',
 };
