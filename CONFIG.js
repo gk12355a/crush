c72e045cde11ebe476a007acbf908edc7bcf5924
gk12355a <99313253+gk12355a@users.noreply.github.com>
2023-06-14 21:11:34 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Hi HUYỀN </br> Chúc quỳnh 8/3 vui vỏe  :3",
+  titleHeader: "Hi LANH  </br> Have a good day  :3",
   descriptionHeader:
-    "Đọc chút tâm tư của tôi đi^^",
+    "Làm ny tớ nhé^",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "hí hí ",
   descriptionModar:
-    ' "Ngày còn lowkey, ít quan hệ, không chơi bời, dù có thích ai cũng khó thành chuyện yêu đương, nhát, nghĩ rằng nếu mà highkey hơn chắc sẽ tốt<\br>Ngày chọn highkey, xã giao nhiều, thích là có quan hệ, không lêu lổng nhưng cũng chẳng chịu ở nhà, thế mà vẫn cứ thích người xưa<\br>Không nổi bật nhưng cũng chẳng vô danh, vậy mà trái tim lại không đổi, hoá ra là nhận định sai, vốn dĩ ở bản thân mình, người ta đã không thích, lên voi xuống chó ba chìm bảy nổi vẫn là không thích. Chỉ là, tưởng được nhiều người biết đến thì sẽ xuất hiện nhiều người như em thôi, nhưng em quá đỗi đặc biệt, nhỉ? "',
+    ' "Lên xe đi lượn 1 vòng qua tim tớ khum "',
 };
