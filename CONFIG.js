44dfebac241749fd89aab85b604ebf2c1c234f51
gk12355a <99313253+gk12355a@users.noreply.github.com>
2024-02-14 23:29:11 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Hi Minh Anh  </br> Have a good day  :3",
+  titleHeader: "Hi Huỳn cute cụa tớ  </br> Valentine vui vẻ <3",
   descriptionHeader:
-    "Làm vợ tớ nhé^",
+    "Sau này hãy để mình nấu cơm nhé",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "hí hí ",
+  titleModar: "Bít ngay là huyền sẽ đồng ý mà",
   descriptionModar:
-    ' "Lên xe đi lượn 1 vòng qua tim tớ khum "',
+    ' "Mình cảm ơn vì đã xuất hiện trong thanh xuân của mình</br> Huyền giống như ánh dương soi sáng cuộc đời mình vậy</br> Mình cũng xin lỗi vì đã có những lúc nặng lời với cậu</br> Mình thề là sẽ không như thế trong tương lai nữa</br> Mong sao cho mọi thứ cứ bình yên như bây giờ thì hay biết mấy"',
 };
